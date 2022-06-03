043c360203e3090bf5923af07df9b79c55361e64
Teja_Veeramachaneni <tejaveeramachaneni15@gmail.com>
2022-06-04 00:29:42 +0530

    working on state and set state

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import Hello from './components/Hello';
 import Greet from './components/Greet';
+import Message from './components/Message';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Greet name = "Bruce" />
-      <Greet name = "Clark" />
-        <button>Click Button</button>
-      <Greet />
-      <Greet name = "Diana" heroName = "wonderwoman" />
-      <p>This is children props</p><Greet /> 
+      {/* <Greet name = "Bruce" /> */}
+      {/* <Greet name = "Clark" /> */}
+        {/* <button>Click Button</button> */}
+      {/* <Greet /> */}
+      {/* <Greet name = "Diana" heroName = "wonderwoman" /> */}
+      {/* <p>This is children props</p><Greet />  */}
+      <Message />
     </div>
   )
 }
