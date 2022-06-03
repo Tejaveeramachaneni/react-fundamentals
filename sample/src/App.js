5c09b16d91b988344625b445245099e1b02fbeff
Teja_Veeramachaneni <tejaveeramachaneni15@gmail.com>
2022-06-03 23:25:30 +0530

    render html using jsx

---
+++ sample/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Hello from './components/Hello';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
