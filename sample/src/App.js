ead30207b2997cc7dc8bb3066c764c891c8aa1f5
Teja_Veeramachaneni <tejaveeramachaneni15@gmail.com>
2022-06-04 00:14:27 +0530

    working on react props

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import Hello from './components/Hello';
+import Greet from './components/Greet';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Hello />
+      <Greet name = "Bruce" />
+      <Greet name = "Clark" />
+        <button>Click Button</button>
+      <Greet />
+      <Greet name = "Diana" heroName = "wonderwoman" />
+      <p>This is children props</p><Greet /> 
     </div>
-  );
+  )
 }
 
 export default App;
